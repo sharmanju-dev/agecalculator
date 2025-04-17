72acf445061274d14d75bf979f0b601518ad4eb2
sharmanju-dev <as2340319@outlook.com>
2025-04-18 00:00:14 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,61 @@
+//swap two number
+// let a = 3;
+// let b =2;
+// let c;
+
+// c= a;
+// a=b;
+// b=c;
+// console.log(a,b);
+
+// let Name="anju";
+// console.log(typeof Name); //typeof is used to check variable type
+
+// let num=20;
+// let str= Number(num);
+// let shree=String(num);
+// console.log(str, shree);
+
+// let r= 40;
+// let areaOfCircle= Math.PI* r *r;
+// console.log(areaOfCircle);
+
+
+// let val=prompt("enter a number");
+// if (val % 3 == 0 ) {
+//     console.log("FizzBuzz");
+// }else if(val % 5 ==0){
+//     console.log("Not FizzBuzz");
+// }else{
+//     console.log("not divisible by 3 and 5");
+// }
+    
+
+// function add(a,b){
+//     return a+b;
+// }
+
+
+// console.log(add(4,5));
+
+// function str(a){
+//     return a.split("").reverse().join("");
+
+// }
+// console.log(str("hello world"));
+// console.log(str("Anju"));
+
+
+// function factorial(a){
+//     result=1;
+//     for (let i = 2; i >= a; i--) {
+//         result*=i;
+//     }
+// }
+// console.log(factorial(5)); // 
+
+const square = (x) =>{
+    return x*x;
+}
+
+console.log(square(prompt("enter a number")));
