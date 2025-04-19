3cc89fbf6d9053336fd0642ab538e441fc8cd966
sharmanju-dev <as2340319@outlook.com>
2025-04-20 03:22:15 +0530

    first commit

--- first.js
+++ first.js
@@ -1,46 +1,17 @@
-// setTimeout(() => {
-//     console.log("hello world");
-// }, 2000);
+    
+     function calculateAge() { 
+        let birthYear = document.getElementById("birthYear").value;
+        let currentYear = new Date().getFullYear();
+         
+        
+        if(birthYear === "" || birthYear > currentYear) {
+            console.log("enter a correct year");
+            document.getElementById("result").innerText = "Please enter a valid birth year.";
+            return;
+        }
+        else{
+        let age = currentYear-birthYear;
+        document.getElementById("result").innerText = `You are ${age} years old.`;
+    }
 
-// setInterval(() =>{
-//     const now = new Date();
-//     console.log(now.toLocaleTimeString());
-// },1000
-// );
-
-
-// const myPromise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve("Resolved after 3 seconds!");
-//     }, 3000);
-//   });
-  
-//   myPromise.then((message) => {
-//     console.log(message);
-//   });
- 
-
-// const form = document.getElementById("myForm");
-// const output = document.getElementById("output");
-
-//   form.addEventListener("submit", function(event) {
-//     event.preventDefault(); // prevent page reload
-//     const name = document.getElementById("username").value;
-//     output.textContent = "You entered: " + name;
-//   });
-
-function delayPromise() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Done waiting!");
-    }, 2000);
-  });
 }
-
-async function callAsync() {
-  console.log("Waiting...");
-  const result=await delayPromise();
-  console.log(result);
-}
-
-callAsync();
